01d422d0634543f56a959c6f5fd3ea00253d6feb
Alex Andrango <arandrangoa@uce.edu.ec>
2023-01-24 15:48:01 -0500

    Tarea 8 Mapeo de relaciones OneToMany y ManyToOne

--- src/main/java/com/example/demo/ProyectoPaU2AaApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU2AaApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,10 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.demo.uce.modelo.Ciudadano;
 import com.example.demo.uce.modelo.Empleado;
 import com.example.demo.uce.modelo.Estudiante;
+import com.example.demo.uce.modelo.Habitacion;
+import com.example.demo.uce.modelo.Hotel;
 import com.example.demo.uce.modelo.Profesor;
 import com.example.demo.uce.service.ICiudadanoService;
 import com.example.demo.uce.service.IEmpleadoService;
 import com.example.demo.uce.service.IEstudianteService;
+import com.example.demo.uce.service.IHotelService;
 import com.example.demo.uce.service.IProfesorService;
 
 @SpringBootApplication
@@ -25,6 +30,9 @@ public class ProyectoPaU2AaApplication implements CommandLineRunner{
 	
 	@Autowired
 	private IEmpleadoService empleadoService;
+	
+	@Autowired
+	private IHotelService hotelService;
 	
 	
 	public static void main(String[] args) {
@@ -61,6 +69,33 @@ public class ProyectoPaU2AaApplication implements CommandLineRunner{
 		empl2.setCiudadano(ciu2);
 		
 		this.ciudadanoService.guardar(ciu2);*/
+		Habitacion habi1=new Habitacion();
+		Habitacion habi2=new Habitacion();
+		Habitacion habi3=new Habitacion();
+		
+		
+		List<Habitacion> listaHabitaciones=new ArrayList<>();
+		listaHabitaciones.add(habi1);
+		listaHabitaciones.add(habi2);
+		listaHabitaciones.add(habi3);
+		
+		
+		Hotel hotel=new Hotel();
+		hotel.setDireccion("Montañita");
+		hotel.setNombre("Decameron");
+		hotel.setHabitaciones(listaHabitaciones);
+		
+		Hotel actualihotel=this.hotelService.encontrar(2);
+		actualihotel.setDireccion("Atacames");
+		actualihotel.setHabitaciones(listaHabitaciones);
+		actualihotel.setNombre("Diamond");
+		
+		this.hotelService.agregar(hotel);
+		this.hotelService.borrar(1);
+		this.hotelService.modificar(actualihotel);
+		
+		
+		
 		
 		
 		
